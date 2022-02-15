daef4f343e697938c5737da2cbbd26a8106b8295
ujin_bang <qkddnwls14@gmail.com>
2022-02-15 16:51:06 +0900

    Intent로 화면 이동

--- app/src/main/java/com/app40/intent_20220215/MainActivity.kt
+++ app/src/main/java/com/app40/intent_20220215/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.app40.intent_20220215
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnMoveToOtherActivity.setOnClickListener {
+
+//            버튼이 눌리면 OtherActivity로 이동
+
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
     }
 }
