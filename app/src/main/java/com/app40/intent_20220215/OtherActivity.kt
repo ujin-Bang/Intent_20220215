2b3d62a66cf6a99352bce22f5bddc73672b3b183
ujin_bang <qkddnwls14@gmail.com>
2022-02-15 16:59:27 +0900

    finish()로 이전 화면으로 복귀

--- app/src/main/java/com/app40/intent_20220215/OtherActivity.kt
+++ app/src/main/java/com/app40/intent_20220215/OtherActivity.kt
@@ -1,11 +1,24 @@
 package com.app40.intent_20220215
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        btnReturnToMain.setOnClickListener {
+
+//            돌아가는 경우에는 Intent를 활용하지 않는다.
+
+
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+            finish()
+
+        }
     }
 }
